292d8a0aef6990f8e79ecb4b026a190b5b694587
立瑜 <baredragon0@gmail.com>
2024-08-12 17:43:52 +0800

    更新algorithm_learning資料夾路徑

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  
   plugins: [react()],
+  base: '/algorithm_learning/',
   resolve: {
         alias: {
           '@': path.resolve(__dirname, './src'),
