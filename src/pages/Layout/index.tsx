8c60c52c351e2ea9e47b02344e613232b5390b3b
立瑜 <baredragon0@gmail.com>
2024-08-12 17:28:07 +0800

    路由配置更改

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -35,7 +35,7 @@ const { Header, Sider, Footer } = Layout
 
 const items: MenuItem[] = [
     getItem('首頁', '/', <HomeOutlined />),
-    getItem('演算法學習', 'sub1', <DiffOutlined />, [
+    getItem('演算法', 'sub1', <DiffOutlined />, [
       getItem('第一章-時間複雜度', 'TimeComplexity'),
     //   getItem('Bill', '4'),
     //   getItem('Alex', '5'),
