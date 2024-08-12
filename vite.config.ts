432d7a0a17de6928416012ca77ee741f51b13c26
立瑜 <baredragon0@gmail.com>
2024-08-12 18:19:57 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path'
 export default defineConfig({
   
   plugins: [react()],
-  base: '/algorithm_learning/',
+  base: './',
   resolve: {
         alias: {
           '@': path.resolve(__dirname, './src'),
